5cf321279bd657b4cc9d0a823b4e5ee242dcea6c
wocs <joonhwan12s@naver.com>
2023-06-23 17:56:45 +0900

    20230623스프링 통합 테스트

--- src/main/java/hello/hellospring/SpringConfig.java
+++ src/main/java/hello/hellospring/SpringConfig.java
@@ -1,13 +1,24 @@
 package hello.hellospring;
 
+import hello.hellospring.repository.JdbcMemberRepository;
 import hello.hellospring.repository.MemberRepository;
 import hello.hellospring.repository.MemoryMemberRepository;
 import hello.hellospring.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import javax.sql.DataSource;
+
 @Configuration//스프링빈을 직접 등록하기위한 방법
 public class SpringConfig {
+
+    private DataSource dataSource;
+
+    @Autowired
+    public SpringConfig(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
 
     @Bean//memberservice와 memberRepository를 둘다 스프링빈에 등록하고 spring bean에 등록되어 있는 memberRepository를 memberservice에 넣어준다.
     public MemberService memberService(){
@@ -16,6 +27,6 @@ public class SpringConfig {
     //스프링이 올라올때 memberservice를 스프링 컨테이너에 올리고 memberRepositroy도 Bean을 보고 스프링빈에 올려준다. 그러면서 memberservice에 memberRepositroy를 사용할 수 있도록 호출해준다.
     @Bean
     public MemberRepository memberRepository(){
-        return new MemoryMemberRepository();
+        return new JdbcMemberRepository(dataSource);
     }
 }
---
+++ src/test/java/hello/hellospring/service/MemberServiceIntegrationTest.java
@@ -0,0 +1,74 @@
+package hello.hellospring.service;
+
+import hello.hellospring.domain.Member;
+import hello.hellospring.repository.MemoryMemberRepository;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+@SpringBootTest
+@Transactional
+class MemberServiceIntegrationTest {
+
+    MemberService memberService;
+    MemoryMemberRepository memberRepository;//중복검사 이후 저장된 값을 클리어해주기위해 Reposityory를 불러와 다 끝나고나면 DB의 값을 날려준다.
+    //MemoryMemberRepository memberRepository = new MemoryMemberRepository();
+    @BeforeEach//각 테스트를 실행하기전에 해준다.
+    public void beforeEach(){
+        memberRepository = new MemoryMemberRepository();//같은 memoryMemberRepository를 사용하기 위함 이렇게 memberReposiroty객체는 static이 사용되지 않는다면 문제가 된다. 이렇게 바꿔줌으로 의존성 주입을 한다.
+        memberService = new MemberService(memberRepository);
+    }
+    @AfterEach//메서드가 끝날때마다 실행되는 함수
+    public void afterEach(){
+        memberRepository.clearStore();
+    }
+    @Test
+    void 회원가입() {
+        //given 상황이 주어졌을 때
+        Member member = new Member();
+        member.setName("hello");
+
+        //when 이것을 실행했을 때
+        Long saveId = memberService.join(member);
+
+        //then 이러한 결과가 나와야 한다.
+        Member findNumber = memberService.findOne(saveId).get();
+        assertThat(member.getName()).isEqualTo(findNumber.getName());
+
+    }
+    @Test
+    public void 중복_회원_예외(){
+        //given 이런 상황이 주어졌을 때
+        Member member1 = new Member();
+        member1.setName("spring");
+
+        Member member2 = new Member();
+        member2.setName("spring");
+
+        //when 이것을 실행했을 때
+        memberService.join(member1);//아래 join 로직을 실행하면 아래의 예외가 터지는지 검사(첫번째 인자에는 기대하는 예외 클래스, 두번째 인자에는 예외를 발생하는 코드를 작성
+        IllegalStateException e = assertThrows(IllegalStateException.class, () -> memberService.join(member2));//위 로직을 실행하면 뒤 예외타입과 같은지 혹은 터지는지 검사
+        //then 이러한 결과가 나와야한다.
+        assertThat(e.getMessage()).isEqualTo("이미 존재하는 회원 입니다.");//이러한 예외가 터지면(문자일치여부)
+
+//        try{
+//            memberService.join(member2);
+//            fail();
+//        } catch (IllegalStateException e){
+//            assertThat(e.getMessage()).isEqualTo("이미 존재하는 회원 입니다.");
+//        }
+    }
+
+    @Test
+    void findMembers() {
+    }
+
+    @Test
+    void findOne() {
+    }
+}
